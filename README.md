76e1140ea200143a1100208a127e960c9f71d352
AbstractNucleus <noelhkleen@gmial.com>
2022-06-25 14:05:15 +0200

    .

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# NextTemplate
-Should be used as a starter template for a nextjs application using prisma, tailwindcss, next-auth and fontawesome icons.
-
-It has some custom scripts that shortens the code in the main files.
+# A website for Släps Gymnastikförening
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,25 +1,46 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prisma";
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    })
+    GoogleProvider({
+      // @ts-ignore
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      // @ts-ignore
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
+  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.SECRET,
+  },
+  //pages: {
+  // signIn: '/auth/signin',  // Displays signin buttons
+  // signOut: '/auth/signout', // Displays form with sign out button
+  // error: '/auth/error', // Error code passed in query string as ?error=
+  // verifyRequest: '/auth/verify-request', // Used for check email page
+  // newUser: null // If set, new users will be directed here on first sign in
+  //},
   callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
     },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken
-      return session
-    }
-  }
-})
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token;
+    },
+  },
+  events: {},
+  debug: false,
+});
